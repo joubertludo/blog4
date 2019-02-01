01f046f8b3a0d21c3c7cb40a0108113d3a9c57e0
joubertludo <jludovic@icloud.com>
2019-02-01 15:12:43 +0100

    nnnnn

--- views/signinform.php
+++ views/signinform.php
@@ -3,20 +3,20 @@
   <div class="form-row">
     <div class="col-md-4 mb-3">
       <label for="validationCustom01" class="text-white">First name</label>
-      <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
+      <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Paul" required>
       <div class="valid-feedback">
         Looks good!
       </div>
     </div>
     <div class="col-md-4 mb-3">
-      <label for="validationCustom02">Last name</label>
-      <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
+      <label for="validationCustom02" class="text-white">Last name</label>
+      <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Desrine" required>
       <div class="valid-feedback">
         Looks good!
       </div>
     </div>
     <div class="col-md-4 mb-3">
-      <label for="validationCustomUsername">Username</label>
+      <label for="validationCustomUsername" class="text-white">Email</label>
       <div class="input-group">
         <div class="input-group-prepend">
           <span class="input-group-text" id="inputGroupPrepend">@</span>
@@ -27,35 +27,20 @@
         </div>
       </div>
     </div>
-  </div>
-  <div class="form-row">
-    <div class="col-md-6 mb-3">
-      <label for="validationCustom03">City</label>
-      <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
-      <div class="invalid-feedback">
-        Please provide a valid city.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationCustom04">State</label>
-      <input type="text" class="form-control" id="validationCustom04" placeholder="State" required>
-      <div class="invalid-feedback">
-        Please provide a valid state.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationCustom05">Zip</label>
-      <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
-      <div class="invalid-feedback">
-        Please provide a valid zip.
-      </div>
-    </div>
-  </div>
+    <div class='form-group col-md-6'>
+              <label for='userPass' class="text-white" >Mot de passe</label>
+              <input name='mdp' type='password' class='form-control' id='inputPassword4' placeholder='Password'>
+            </div>
+            <div class='form-group col-md-6'>
+              <label for='userPass' class="text-white">confirmer mot de passe</label>
+              <input name='mdp' type='password' class='form-control' id='inputPassword4' placeholder='Password'>
+            </div>
+  
   <div class="form-group">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
       <label class="form-check-label" for="invalidCheck">
-        Agree to terms and conditions
+      
       </label>
       <div class="invalid-feedback">
         You must agree before submitting.
@@ -63,5 +48,6 @@
     </div>
   </div>
   <button class="btn btn-primary" type="submit">Submit form</button>
+</div>
 </div>
 </form>
