283d0acb4b9076e49ffde0196dcd8ef639e32fee
flematthias <37439367+flematthias@users.noreply.github.com>
2019-02-06 16:57:28 +0100

    gg

--- views/admin.php
+++ views/admin.php
@@ -1,33 +1,37 @@
 
-<section class="container bg-info">
-    
-<table class='table '>
- <thead>
-   <tr>
-     <th scope='col'>Catégorie</th>
-     <th scope='col'>Nombre de posts</th>
-     <th scope='col'>Premier article publié</th>
-     <th scope='col'>Date</th>
-   </tr>
- </thead>
- <tbody>
-<?php foreach ($search_all_categories as $datas) {
- echo" 
-   <tr>
-     <th scope='row'>".utf8_encode($datas['name'])."</th>
-     <td>".utf8_encode($datas['count(*)'])."</td>
-     <td>".utf8_encode($datas['title'])."</td>
-     <td>".utf8_encode($datas['updated_date'])."</td>
-   </tr>
-   
- ";
-
-
-  } ?>
-</tbody>
-</table>
-
-
-   <hr class="featurette-divider">
+<section class="p-5 bg-light">
+<h2 class="pb-5 text-center">Administrer</h2>
+  <div class="container bg-light shadow mb-5 bg-white rounded">
+      
+  <table class='table '>
+   <thead>
+     <tr>
+       <th scope='col'>Catégorie</th>
+       <th scope='col'>Nombre de posts</th>
+       <th scope='col'>Premier article publié</th>
+       <th scope='col'>Date</th>
+     </tr>
+   </thead>
+   <tbody>
+  <?php foreach ($search_all_categories as $datas) {
+   echo" 
+     <tr>
+       <th scope='row'>".utf8_encode($datas['name'])."</th>
+       <td>".utf8_encode($datas['count(*)'])."</td>
+       <td>".utf8_encode($datas['title'])."</td>
+       <td>".date("d-m-Y",strtotime($datas['updated_date']))."</td>
+       
+     </tr>
+     
+   ";
+  
+  
+    } ?>
+  </tbody>
+  </table>
+  
+  
+     <hr class="featurette-divider">
+  </div>
 </section>
 
